43ac3a23080929e35fe093c0f34244b455665ef2
Juan Pablo Cervantes Gonzalez <cer11glez@gmail.com>
2020-02-24 01:02:22 -0600

    Add simple server

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send(`express`)
+})
+
+app.listen(port, err => {
+    if (err) {
+        return console.log(`Ocurrio un error ${err}`);
+    }
+    console.log(`http://localhost:${port}`);
+})
